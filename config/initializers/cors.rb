429b11e8cdf10c884ab17e714ca8b415f81f01df
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-04-30 12:18:10 -0400

    configuring rack-cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3000'
+    resource '*', headers: :any, methods:
+    %i[get post put patch delete options head], credentials: true
+  end
+  allow do
+    origins 'https://arn-react-redux-bookstore.netlify.app/'
+    resource '*', headers: :any, methods:
+    %i[get post put patch delete options head], credentials: true
+  end
+  allow do
+    origins 'https://arn-bookstore-frontend.herokuapp.com/'
+    resource '*', headers: :any, methods:
+    %i[get post put patch delete options head], credentials: true
+  end
+end
