fdd10e1c96e3e5f1467ec51dde0314dcb20cfc13
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-04-30 13:09:20 -0400

    rack cors origins removed * wildcard

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     %i[get post put patch delete options head], credentials: true
   end
   allow do
-    origins '*'
+    origins 'https://arn-react-redux-bookstore.netlify.app/'
     resource '*', headers: :any, methods:
-    %i[get post put patch delete options head]
+    %i[get post put patch delete options head], credentials: true
   end
   allow do
     origins 'https://arn-bookstore-frontend.herokuapp.com/'
