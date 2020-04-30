2d98890bd97384baffc4603f390e20ac5ae317d6
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-04-30 12:42:21 -0400

    session_store.rb configure

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
-
-Rails.application.config.session_store :cookie_store,
-                                       key: '_authentication_app',
-                                       domain: 'arn-bookstore-backend.herokuapp.com'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store,
+                                        key: '_authentication_app',
+                                        domain: 'arn-bookstore-backend.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store,
+                                       key: '_authentication_app'
+end
