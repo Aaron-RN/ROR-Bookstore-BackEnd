797fabca7f3de1d72dc2a9ae81f4c6cee4af6b12
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-04-30 13:00:49 -0400

    rack-cors origins set to *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*', headers: :any, methods:
-    %i[get post put patch delete options head], credentials: true
+    %i[get post put patch delete options head]
   end
   allow do
     origins 'https://arn-bookstore-frontend.herokuapp.com/'
